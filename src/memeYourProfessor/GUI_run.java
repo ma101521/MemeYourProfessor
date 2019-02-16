d464065a6606caa290c15fe908a4c78b8977c89d
Marisa <ma101521@live.unc.edu>
2019-02-16 14:07:57 -0500

    updated 2:07

--- src/memeYourProfessor/GUI_run.java
+++ src/memeYourProfessor/GUI_run.java
@@ -1,10 +1,26 @@
 package memeYourProfessor;
 
+import java.awt.BorderLayout;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
 public class GUI_run {
 
 	public static void main(String[] args) {
 		//This will be where we launch the GUI
 
+			JFrame main_frame = new JFrame();
+			main_frame.setTitle("Meme Your Professor");
+			main_frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+			
+			JPanel top_panel = new JPanel();
+			top_panel.setLayout(new BorderLayout());
+			//top_panel.add(view, BorderLayout.CENTER);
+			main_frame.setContentPane(top_panel);
+
+			main_frame.pack();
+			main_frame.setVisible(true);
 	}
 
 }
