df5513cc2ec1d707eb4a9d106c60f92f6eb3e407
Marisa <ma101521@live.unc.edu>
2019-02-16 21:41:20 -0500

    all changes

--- src/memeYourProfessor/GUI_view.java
+++ src/memeYourProfessor/GUI_view.java
@@ -22,6 +22,7 @@ import javax.swing.event.ChangeListener;
 public class GUI_view extends JPanel implements MouseListener{
 	
 	private JPanel full_image;
+	private String thumbnail_flag;
 
 	public GUI_view() throws IOException {
 		
@@ -42,9 +43,9 @@ public class GUI_view extends JPanel implements MouseListener{
 		panel1.add(caption1, BorderLayout.SOUTH);
 		panel1.addMouseListener(new MouseAdapter() { 
 	          public void mousePressed(MouseEvent me) { 
-	            System.out.println("Click works"); 
 	            full_image.removeAll();
 	            full_image.add(thumb1, BorderLayout.CENTER);
+	            thumbnail_flag = "Kris";
 	            full_image.revalidate();
 	            full_image.repaint();
 	          } 
@@ -57,6 +58,15 @@ public class GUI_view extends JPanel implements MouseListener{
 		JLabel caption2 = new JLabel("KMP - Comp Sci");
 		panel2.add(thumb2, BorderLayout.CENTER);
 		panel2.add(caption2, BorderLayout.SOUTH);
+		panel2.addMouseListener(new MouseAdapter() { 
+	          public void mousePressed(MouseEvent me) { 
+	            full_image.removeAll();
+	            full_image.add(thumb2, BorderLayout.CENTER);
+	            thumbnail_flag = "KMP";
+	            full_image.revalidate();
+	            full_image.repaint();
+	          } 
+	        }); 
 		thumbnails.add(panel2);
 		
 		JPanel panel3 = new JPanel(new BorderLayout());
@@ -65,6 +75,15 @@ public class GUI_view extends JPanel implements MouseListener{
 		JLabel caption3 = new JLabel("Joaquin Drut - Physics");
 		panel3.add(thumb3, BorderLayout.CENTER);
 		panel3.add(caption3, BorderLayout.SOUTH);
+		panel3.addMouseListener(new MouseAdapter() { 
+	          public void mousePressed(MouseEvent me) { 
+	            full_image.removeAll();
+	            full_image.add(thumb3, BorderLayout.CENTER);
+	            thumbnail_flag = "Joaquin";
+	            full_image.revalidate();
+	            full_image.repaint();
+	          } 
+	        }); 
 		thumbnails.add(panel3);
 		
 		JPanel panel4 = new JPanel(new BorderLayout());
@@ -73,6 +92,15 @@ public class GUI_view extends JPanel implements MouseListener{
 		JLabel caption4 = new JLabel("Sean Washberg - Physics");
 		panel4.add(thumb4, BorderLayout.CENTER);
 		panel4.add(caption4, BorderLayout.SOUTH);
+		panel4.addMouseListener(new MouseAdapter() { 
+	          public void mousePressed(MouseEvent me) { 
+	            full_image.removeAll();
+	            full_image.add(thumb4, BorderLayout.CENTER);
+	            thumbnail_flag = "Sean";
+	            full_image.revalidate();
+	            full_image.repaint();
+	          } 
+	        }); 
 		thumbnails.add(panel4);
 		
 		JPanel panel5 = new JPanel(new BorderLayout());
@@ -81,6 +109,15 @@ public class GUI_view extends JPanel implements MouseListener{
 		JLabel caption5 = new JLabel("Amy Oldenburg - Physics");
 		panel5.add(thumb5, BorderLayout.CENTER);
 		panel5.add(caption5, BorderLayout.SOUTH);
+		panel5.addMouseListener(new MouseAdapter() { 
+	          public void mousePressed(MouseEvent me) { 
+	            full_image.removeAll();
+	            full_image.add(thumb5, BorderLayout.CENTER);
+	            thumbnail_flag = "Amy";
+	            full_image.revalidate();
+	            full_image.repaint();
+	          } 
+	        }); 
 		thumbnails.add(panel5);
 		
 		JPanel panel6 = new JPanel(new BorderLayout());
@@ -89,6 +126,15 @@ public class GUI_view extends JPanel implements MouseListener{
 		JLabel caption6 = new JLabel("Jack Ng - Physics");
 		panel6.add(thumb6, BorderLayout.CENTER);
 		panel6.add(caption6, BorderLayout.SOUTH);
+		panel6.addMouseListener(new MouseAdapter() { 
+	          public void mousePressed(MouseEvent me) { 
+	            full_image.removeAll();
+	            full_image.add(thumb6, BorderLayout.CENTER);
+	            thumbnail_flag = "Jack";
+	            full_image.revalidate();
+	            full_image.repaint();
+	          } 
+	        }); 
 		thumbnails.add(panel6);
 		
 		JPanel panel7 = new JPanel(new BorderLayout());
@@ -97,6 +143,15 @@ public class GUI_view extends JPanel implements MouseListener{
 		JLabel caption7 = new JLabel("Christian Illiadis - Physics");
 		panel7.add(thumb7, BorderLayout.CENTER);
 		panel7.add(caption7, BorderLayout.SOUTH);
+		panel7.addMouseListener(new MouseAdapter() { 
+	          public void mousePressed(MouseEvent me) { 
+	            full_image.removeAll();
+	            full_image.add(thumb7, BorderLayout.CENTER);
+	            thumbnail_flag = "Christian";
+	            full_image.revalidate();
+	            full_image.repaint();
+	          } 
+	        }); 
 		thumbnails.add(panel7);
 		
 		JPanel panel8 = new JPanel(new BorderLayout());
@@ -105,6 +160,15 @@ public class GUI_view extends JPanel implements MouseListener{
 		JLabel caption8 = new JLabel("David Plaisted - Comp Sci");
 		panel8.add(thumb8, BorderLayout.CENTER);
 		panel8.add(caption8, BorderLayout.SOUTH);
+		panel8.addMouseListener(new MouseAdapter() { 
+	          public void mousePressed(MouseEvent me) { 
+	            full_image.removeAll();
+	            full_image.add(thumb8, BorderLayout.CENTER);
+	            thumbnail_flag = "David";
+	            full_image.revalidate();
+	            full_image.repaint();
+	          } 
+	        }); 
 		thumbnails.add(panel8);
 		
 		
